a1ff05d67996f69d1845fc475a63c06757af5b91
Angel-enc <enock8876@gmail.com>
2025-07-22 12:09:33 +0000

    inicio formulario

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
+	const{store,dispatch} = useGlobalReducer()
 
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					
+
+
+
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="/AñadirContacto">
+						<button className="btn btn-primary">Añadir nuevo contacto</button>
 					</Link>
 				</div>
 			</div>
